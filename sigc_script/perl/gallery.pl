81430e836b1753387dec3332fe5aac3d62122246
ben.lavery <devnull@localhost>
2010-07-13 15:33:54 +0100

    Added global variables (mainly copied from old Bash script) and added GetOption statements.

--- sigc_script/perl/gallery.pl
+++ sigc_script/perl/gallery.pl
@@ -6,7 +6,7 @@
 
 # First created on 15/05/2010
 # Perl script created on 08/07/2010
-# Last edited on 08/07/2010
+# Last edited on 12/07/2010
 
 # Project site:
 # http://code.google.com/p/simple-image-gallery-creator/
@@ -51,9 +51,51 @@ use Getopt::Long;
 
 
 ###############################################################################
-################################ Global variables #############################
+################################ "Global" variables ###########################
 ###############################################################################
+####FILE SETTINGS####
+$INDEX_EXTENTION = "php";
 
+####GALLERY SETTINGS####
+$CAPTIONS = 0;
+$TITLE = undef;
+$GENERATE_TITLE = 0;
+$COLUMNS = 4;
+$LIGHTBOX = 0;
+$LIGHTBOX_GROUP = 0;
+$DESCRIPTION = undef;
+
+#####HTML SETTINGS####
+$FULL_HTML = 0;
+$HTML4 = "HTML4.01";
+$XHTML1 = "XHTML1.0";
+$XHTML11 = "XHTML1.1";
+
+####IMAGE SETTINGS####
+$THUMB_PIXELS = 128;
+$BIG_PIXELS = 640;
+@IMAGES = undef;
+
+####SCRIPT SETTINGS####
+$QUIET = 0;
+$EXIT_STATUS = 0;
+$EXIT_REASON = undef;
+$SEARCH_PATH = undef;
+$HELP = 0;
+$VERSION = 0;
+
+####DIRECTORIES & FILES####
+$ROOT = "";
+$THUMBS = "$ROOT/thumbs";
+$BIGS = "$ROOT/bigs";
+$INDEX_FILE = "index.$INDEX_EXTENTION";
+$INDEX = "$ROOT/$INDEX_FILE";
+$NEWPATH = "";
+$SOURCE = "";
+$RELATIVE = "";
+$DIRECTORY = "";
+$DESCRIPTION_FILE = "$ROOT/description.txt";
+$STORE = "$ROOT/.store";
 ###############################################################################
 
 
@@ -61,6 +103,16 @@ use Getopt::Long;
 ################################ Cleanup Function #############################
 ###############################################################################
 sub cleanup{
+	#Do nothing so far
+}
+
+###############################################################################
+
+
+###############################################################################
+################################ Help Function ################################
+###############################################################################
+sub help{
 	#Do nothing so far
 }
 
@@ -81,8 +133,26 @@ sub cleanup{
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\MAIN/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/#
 ###############################################################################
 
-#### Check dependancies ####
 #### Collect arguments ####
+GetOptions('B|big_size=s' => \$BIG_PIXELS);
+GetOptions('c|columns:i' => \$COLUMNS);
+GetOptions('C|captions' => \$CAPTIONS);
+GetOptions('d|gallery_directory=s' => \$DIRECTORY);
+GetOptions('D|description_location=s' => \$DESCRIPTION);
+GetOptions('description_file=s' => \$DESCRIPTION_FILE);
+GetOptions('e|extention=s' => \$INDEX_EXTENTION);
+GetOptions('h|help|?' => \$HELP);
+GetOptions('H|standalone_html_version=s' => \$FULL_HTML);
+GetOptions('l' => \$LIGHTBOX);
+GetOptions('L' => \$LIGHTBOX_GROUP);
+GetOptions('q|quiet' => \$QUIET);
+GetOptions('r|relative_to=s' => \$RELATIVE);
+GetOptions('s|source_directory=s' => \$SOURCE);
+GetOptions('S|thumb_size=i' => \$THUMB_PIXELS);
+GetOptions('t=s' => \$TITLE);
+GetOptions('T' => \$GENERATE_TITLE);
+GetOptions('V' => \$VERSION);
+
 #### Check arguments ####
 #### Check directories ####
 #### Create bigs, thumbs & .store directories ####
